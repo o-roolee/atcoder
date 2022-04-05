cabe6674ff7ddac22e7602819905ebaae3e41e4c
Ohkabe Fumiya <ookabe@43s.co.jp>
2022-04-06 01:56:35 +0900

    wip

--- c++_tutorial/sample.cpp
+++ c++_tutorial/sample.cpp
@@ -1,19 +1,9 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
-
-  int sum = 0; // 合計点を表す変数
-  int x;       // 入力を受け取る変数
-  int i = 0;   // カウンタ変数
-
-  while (i < N) {
-    cin >> x;
-    sum += x;
-    i++;
-  }
-
-  cout << sum << endl;
+  vector<int> vec;
+  vec = { 25, 100, 64 };
+  cout << vec.at(0) << endl;
+  cout << vec.size() << endl;
 }
