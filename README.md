2e8eb8197fd5405e10243d1909ad841d7db97699
Ohkabe Fumiya <ookabe@43s.co.jp>
2022-04-05 14:09:31 +0900

    wip

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # atcoder
+
+## やることメモ
+### C++入門
+https://atcoder.jp/contests/APG4b
+
+### AtCoder Beginners Selection
+https://atcoder.jp/contests/abs
---
+++ abc/245/A.cpp
@@ -0,0 +1,13 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+  int x1, x2, x3, x4;
+  int y1, y2, y3, y4;
+
+  // cin >> x1, y1;
+  // cin >> x2, y2;
+  // cin >> x3, y3;
+
+  cout << 123 << endl;
+}
